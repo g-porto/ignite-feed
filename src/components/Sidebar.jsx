5fe11c0d7da5dd1b5e34bf32cb10f5c10dfe473b
G Porto <giovannaporto96@gmail.com>
2023-12-25 18:40:57 -0300

    cloned project react-fundamentals into this new project that will be the same application but with TypeScript instead

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { PencilSimpleLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYdifHx8fGvufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/g-porto.png" />
+
+        <strong>G Porto</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
